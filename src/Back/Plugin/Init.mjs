fd747209b5e5437073cf7e00c405e3ed8998f3b0
Alex Gusev <alex@flancer64.com>
2022-12-08 10:50:32 +0200

    WF-604 Transfer web authentication into plugin

---
+++ src/Back/Plugin/Init.mjs
@@ -0,0 +1,31 @@
+/**
+ * Plugin initialization function.
+ */
+// MODULE'S VARS
+const NS = 'TeqFw_Db_Back_Plugin_Init';
+
+export default function Factory(spec) {
+    // DEPS
+    /** @type {TeqFw_Db_Back_Defaults} */
+    const DEF = spec['TeqFw_Db_Back_Defaults$'];
+    /** @type {TeqFw_Core_Back_Config} */
+    const config = spec['TeqFw_Core_Back_Config$'];
+    /** @type {TeqFw_Db_Back_RDb_Connect} */
+    const conn = spec['TeqFw_Db_Back_RDb_IConnect$']; // use interface as implementation
+
+    // FUNCS
+    async function action() {
+        // RDB connection
+        /** @type {TeqFw_Db_Back_Dto_Config_Local} */
+        const cfg = config.getLocal(DEF.NAME);
+        if (cfg?.connection)
+            await conn.init(cfg);
+    }
+
+    // MAIN
+    Object.defineProperty(action, 'namespace', {value: NS});
+    return action;
+}
+
+// finalize code components for this es6-module
+Object.defineProperty(Factory, 'namespace', {value: NS});
