c80e52c6fef83a9d22cff95f2ac553771e937130
Alex Gusev <alex@flancer64.com>
2024-05-28 22:04:55 +0300

    lp-desk-8 Add the business entity into the RDB schema
    link to issue: flancer32/lp-desk#8

--- src/Back/Cli/Import.mjs
+++ src/Back/Cli/Import.mjs
@@ -58,13 +58,14 @@ export default function Factory(
                 // prepare RDBMS engine
                 if (trx.isMariaDB()) {
                     // to prevent MariaDB from errors like 'ER_TRUNCATED_WRONG_VALUE'
-                    await trx.getKnexTrx().raw("SET SESSION sql_mode = 'ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';");
+                    await trx.getKnexTrx().raw(`SET SESSION sql_mode = 'ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';`);
                 }
                 // read JSON with data
                 /** @type {TeqFw_Db_Back_Dto_Export.Dto} */
                 const dump = utilFile.readJson(filename);
                 // load DEM and get list of tables in dependency order
                 const demTables = await aDemTables.act();
+                // Process all tables from the current version of the schema in the order of their dependencies.
                 for (const name of demTables) {
                     // prepare the imported data to be inserted into rdb
                     const tableRows = transform.prepareTables(trx, dump.tables, name);
